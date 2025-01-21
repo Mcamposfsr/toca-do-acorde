f78c22578e2fac3a0e7214628329ef2f06215829
Matheus Campos <m.campos612003@gmail.com>
2025-01-21 11:22:28 -0300

    Adicionando arquivos de configuração ao projeto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="flex bg-red-600 w-screen h-screen items-center justify-center">
+        <p className="font-bold text-white text-[400px]">Bom dia</p>
+      </div>
+    </>
+  )
+}
+
+export default App
+  
